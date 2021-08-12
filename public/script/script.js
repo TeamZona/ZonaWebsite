00ff234f103680969419077ebffa4d2ad74dc458
Mayank Kumar <kmayank38@gmail.com>
2021-08-12 20:57:02 +0530

    Added js for about us section

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,18 @@
-const aboutusbtn = document.querySelector("aboutus_btn");
-const aboutuspara = document.querySelector("aboutus_para");
+const aboutusbtn = document.getElementById("aboutus_btn");
+const aboutuspara = document.getElementById("aboutus_description");
+
+const aboutusimg1 = document.getElementById("aboutus_img1");
+const aboutusimg2 = document.getElementById("aboutus_img2");
 
 aboutusbtn.addEventListener('click',() =>{
     aboutuspara.classList.remove('inactive');
+    // aboutuspara.classList.add('aboutus_para');
+    aboutusbtn.classList.add('inactive');
+    aboutusimg1.classList.remove('inactive');
+    // aboutusimg1.classList.add('aboutus_imgonclick1');
+    aboutusimg2.classList.remove('inactive');
+    // aboutusimg2.classList.add('aboutus_imgonclick2');
 } )
+
+
+
