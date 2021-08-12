568d0b3eae037601783e655ed64596c8bfc409f2
Mayank Kumar <kmayank38@gmail.com>
2021-08-12 23:44:43 +0530

    Adding Event Slider UI

--- public/script/script.js
+++ public/script/script.js
@@ -15,4 +15,26 @@ aboutusbtn.addEventListener('click',() =>{
 } )
 
 
+// ----------------------------------------Events--------------------------------------------
+
+let swiper = new Swiper('.swiper-container',{
+
+    cssMode:true,
+
+    loop:true,
+    
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    },
+
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+
+    mousewheel:true,
+    keyboard:true,
+    
+});
 
